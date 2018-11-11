51ae8877457eae40e8226ed8b11efa2c2a61cafa
GiHyunLim <44770414+GiHyunLim@users.noreply.github.com>
2018-11-11 15:58:09 +0900

    find book by name,author,publisher 복붙

--- src/main.c
+++ src/main.c
@@ -1122,7 +1122,20 @@ Client *find_client_by_student_number(const LinkedList *client_list, const wchar
     wprintf("일치하는 학생번호가 없습니다\n"); //current=NULL 임
     return 0;
 }
-LinkedList *find_books_by_name(const LinkedList *book_list, const wchar_t *book_name) { }
+LinkedList *find_books_by_name(const LinkedList *book_list, const wchar_t *book_name) 
+{
+    if (book_list == NULL || book_name == NULL)
+        return 0;
+    
+    LinkedList *result = NULL;
+    
+    for (LinkedList *current = book_list; current != NULL; current = current->next)
+        if (wcscmp(((Book *)current->contents)->name, book_name) == 0)
+            result = insert_book(result, (Book *)current->contents);
+    
+    return result;
+}
+
 LinkedList *find_books_by_ISBN(const LinkedList *book_list, const wchar_t *book_ISBN)
 {
     if (book_list == NULL || book_ISBN == NULL)
@@ -1137,8 +1150,32 @@ LinkedList *find_books_by_ISBN(const LinkedList *book_list, const wchar_t *book_
     return result;
 }
         
-LinkedList *find_books_by_author(const LinkedList *book_list, const wchar_t *book_author) { }
-LinkedList *find_books_by_publisher(const LinkedList *book_list, const wchar_t *book_publisher) { }
+LinkedList *find_books_by_author(const LinkedList *book_list, const wchar_t *book_author)
+{
+    if (book_list == NULL || book_author == NULL)
+        return 0;
+    
+    LinkedList *result = NULL;
+    
+    for (LinkedList *current = book_list; current != NULL; current = current->next)
+        if (wcscmp(((Book *)current->contents)->author, book_author) == 0)
+            result = insert_book(result, (Book *)current->contents);
+    
+    return result;
+}
+LinkedList *find_books_by_publisher(const LinkedList *book_list, const wchar_t *book_publisher) 
+{
+    if (book_list == NULL || book_publisher == NULL)
+        return 0;
+    
+    LinkedList *result = NULL;
+    
+    for (LinkedList *current = book_list; current != NULL; current = current->next)
+        if (wcscmp(((Book *)current->contents)->publisher, book_publisher) == 0)
+            result = insert_book(result, (Book *)current->contents);
+    
+    return result;
+}
 Book *find_book_by_number(const LinkedList *book_list, const wchar_t *book_number) { }
 LinkedList *find_borrows_by_client(const LinkedList *borrow_list, Client *client) { }
 Borrow *find_borrow(const LinkedList *borrow_list, Client *client, Book *book) { }
